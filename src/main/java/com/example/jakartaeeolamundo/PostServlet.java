47cf0e7ad5b47dcd3f781a5ec8cd0b61d4c3a4b3
Jennifer Omena <jennifer.omena@outlook.com>
2022-09-13 22:13:27 -0300

    postservlet

--- src/main/java/com/example/jakartaeeolamundo/PostServlet.java
+++ src/main/java/com/example/jakartaeeolamundo/PostServlet.java
@@ -1,32 +1,48 @@
 package com.example.jakartaeeolamundo;
 
+import com.example.DAO.ConexaoDAO;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 @WebServlet(name = "postServlet", value = "/postServlet")
 public class PostServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
+        try {
+            Connection con = ConexaoDAO.conexaoBD();
+            PreparedStatement st = con.prepareStatement("INSERT INTO usuario (nome, email, senha) VALUES ('jenn', 'jen@email.com', 'senha')");
+            st.executeUpdate();
+            st.close();
+            con.close();
 
-        String nome;
-        String email;
-        String senha;
+            response.setContentType("text/html");
 
-        out.println("<html><body>");
-        out.println("<h1>Cadastro realizado:</h1>");
-        out.println("<p>Nome: "+ request.getParameter("nome") +"</p>");
-        out.println("<p>E-mail: "+ request.getParameter("email") +"</p>");
-        out.println("<p>Senha: "+ request.getParameter("senha") +"</p>");
-        out.println("</body></html>");
+            // Hello
+            PrintWriter out = response.getWriter();
+
+            /*String nome;
+            String email;
+            String senha;
+
+            out.println("<html><body>");
+            out.println("<h1>Cadastro realizado:</h1>");
+            out.println("<p>Nome: "+ request.getParameter("nome") +"</p>");
+            out.println("<p>E-mail: "+ request.getParameter("email") +"</p>");
+            out.println("<p>Senha: "+ request.getParameter("senha") +"</p>");
+            out.println("</body></html>");*/
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
